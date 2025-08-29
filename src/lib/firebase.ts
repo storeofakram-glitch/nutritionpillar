64a6ea7e9c7a096d5ae6cd4974a2fb8efa0c5a77
Store Of akram <storeofakram@gmail.com>
2025-08-29 23:20:39 +0000

    reconnect the firebase project : // Import the functions you need from t

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,16 @@
+
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp, getApps, getApp, type FirebaseApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import type { FirebaseApp } from "firebase/app";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyABOooJEwHAMnqjRxAkyYqtgLgAT3NXEl4",
-  authDomain: "nutrition-pillar-store.firebaseapp.com",
-  projectId: "nutrition-pillar-store",
-  storageBucket: "nutrition-pillar-store.firebasestorage.app",
+  authDomain: "fitness-pillar-store.firebaseapp.com",
+  projectId: "fitness-pillar-store",
+  storageBucket: "fitness-pillar-store.firebasestorage.app",
   messagingSenderId: "580338302966",
   appId: "1:580338302966:web:7586fffbc1b08ab5f211d7",
   measurementId: "G-B5DPZBS8E2"
