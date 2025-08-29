4056ade3d8173a51e3bb29ea0a11befafccc3206
Store Of akram <storeofakram@gmail.com>
2025-08-29 15:44:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'tse2.mm.bing.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
