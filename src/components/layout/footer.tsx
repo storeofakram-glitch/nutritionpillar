8737990482bcc862e0b3ce8b6eaa97af663c63cd
Store Of akram <storeofakram@gmail.com>
2025-08-29 22:27:34 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Dumbbell classNam

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,12 +1,10 @@
-import { Dumbbell } from 'lucide-react';
 import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer className="border-t">
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md-py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Dumbbell className="h-6 w-6 text-primary" />
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
             © {new Date().getFullYear()} Nutrition Pillar. All Rights Reserved.
           </p>
