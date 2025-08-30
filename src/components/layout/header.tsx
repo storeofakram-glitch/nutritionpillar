df37bf4894756fdc7dbce1876ae62154ee357f9a
Store Of akram <storeofakram@gmail.com>
2025-08-30 18:58:40 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -38,10 +38,10 @@ export default function Header() {
         </nav>
         <div className="flex flex-1 items-center justify-end">
           <Button asChild variant="ghost" size="icon">
-            <Link href="/cart">
+            <Link href="/cart" className="relative">
               <ShoppingBag className="h-5 w-5" />
               {cartCount > 0 && (
-                <span className="absolute top-1 right-1 flex h-5 w-5 items-center justify-center rounded-full bg-primary text-xs text-primary-foreground">
+                <span className="absolute -top-2 -right-2 flex h-5 w-5 items-center justify-center rounded-full bg-primary text-xs text-primary-foreground">
                   {cartCount}
                 </span>
               )}
