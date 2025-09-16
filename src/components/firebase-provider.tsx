43844b81ba608b2eba4b69b2c63c51c419465313
Store Of akram <storeofakram@gmail.com>
2025-09-16 12:40:05 +0000

    Try fixing this error: `Runtime TypeError: {imported module [project]/sr

--- src/components/firebase-provider.tsx
+++ src/components/firebase-provider.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { useEffect } from 'react';
@@ -6,12 +7,10 @@ import { analytics } from '@/lib/firebase';
 export default function FirebaseProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     // This effect will run on the client side, after the component mounts.
-    // It resolves the 'analytics' promise and initializes it if supported.
-    analytics.then(instance => {
-      if (instance) {
-        console.log("Firebase Analytics initialized");
-      }
-    });
+    // It checks if the analytics instance was successfully initialized.
+    if (analytics) {
+      console.log("Firebase Analytics initialized");
+    }
   }, []);
 
   return <>{children}</>;
