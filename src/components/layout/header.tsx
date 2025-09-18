6d79c6b1282a9cab06d7ed3572d101aa4a9401ef
Store Of akram <storeofakram@gmail.com>
2025-09-18 16:32:05 +0000

    i see litel space in right an left of the header like the buttons is far

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -65,7 +65,7 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+      <div className="container flex h-14 max-w-screen-2xl items-center px-4">
         <div className="mr-4 flex items-center gap-4 md:hidden">
             <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
                 <SheetTrigger asChild>
